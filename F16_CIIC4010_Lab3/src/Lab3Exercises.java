fc3230b3219e829f675600ca404a1c90717692e4
4015d3 <4015d3@guanabana.ece.uprm.edu>
2016-08-31 14:10:23 -0400

    Added new Class and Changed background color.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,19 +1,14 @@
 import javax.swing.JFrame;
 
-
 public class Lab3Exercises {
-
 	public static void main(String[] args) {
-
+		
 		JFrame myFrame = new JFrame("Jean Candelaria");
-
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
 		myFrame.setLocation(800, 470);
-		
 		myFrame.setSize(200, 200);
-		
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
-
 }
