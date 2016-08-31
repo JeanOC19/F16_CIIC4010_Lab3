d70dee52df3071c116ef98512bc85f045272827d
4015d3 <4015d3@guanabana.ece.uprm.edu>
2016-08-31 13:48:02 -0400

    Renamed frame, changed locations and dimensions.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
 import javax.swing.JFrame;
 
+
 public class Lab3Exercises {
+
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+
+		JFrame myFrame = new JFrame("Jean Candelaria");
+
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		myFrame.setLocation(800, 470);
+		
+		myFrame.setSize(200, 200);
+		
 		myFrame.setVisible(true);
 	}
+
 }
